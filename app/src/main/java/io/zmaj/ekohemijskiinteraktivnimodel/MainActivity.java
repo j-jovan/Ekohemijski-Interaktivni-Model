5a3105c709e4b07c31ed54d73e13a7bd3534904c
darkzmaj <Malapatkica21#>
2019-02-16 15:00:55 +0100

    Initial commit

---
+++ app/src/main/java/io/zmaj/ekohemijskiinteraktivnimodel/MainActivity.java
@@ -0,0 +1,352 @@
+package io.zmaj.ekohemijskiinteraktivnimodel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RadioButton A1 = findViewById(R.id.radioButtonA1);
+        final RadioButton A2 = findViewById(R.id.radioButtonA2);
+        final RadioButton A3 = findViewById(R.id.radioButtonA3);
+        final RadioButton A4 = findViewById(R.id.radioButtonA4);
+        final RadioButton A5 = findViewById(R.id.radioButtonA5);
+        final RadioButton A6 = findViewById(R.id.radioButtonA6);
+        final RadioButton A7 = findViewById(R.id.radioButtonA7);
+        final RadioButton A8 = findViewById(R.id.radioButtonA8);
+        final RadioButton A9 = findViewById(R.id.radioButtonA9);
+        final RadioButton A10 = findViewById(R.id.radioButtonA10);
+        final RadioButton A11 = findViewById(R.id.radioButtonA11);
+        final RadioButton A12 = findViewById(R.id.radioButtonA12);
+        final RadioButton A13 = findViewById(R.id.radioButtonA13);
+        final RadioButton A14 = findViewById(R.id.radioButtonA14);
+        final RadioButton A15 = findViewById(R.id.radioButtonA15);
+        final RadioButton B1 = findViewById(R.id.radioButtonB1);
+        final RadioButton B2 = findViewById(R.id.radioButtonB2);
+        final RadioButton B3 = findViewById(R.id.radioButtonB3);
+        final RadioButton B4 = findViewById(R.id.radioButtonB4);
+        final RadioButton B5 = findViewById(R.id.radioButtonB5);
+        final RadioButton B6 = findViewById(R.id.radioButtonB6);
+        final RadioButton B7 = findViewById(R.id.radioButtonB7);
+        final RadioButton B8 = findViewById(R.id.radioButtonB8);
+        final RadioButton B9 = findViewById(R.id.radioButtonB9);
+        final RadioButton B10 = findViewById(R.id.radioButtonB10);
+        final RadioButton B11 = findViewById(R.id.radioButtonB11);
+        final RadioButton B12 = findViewById(R.id.radioButtonB12);
+        final RadioButton B13 = findViewById(R.id.radioButtonB13);
+        final RadioButton B14 = findViewById(R.id.radioButtonB14);
+        final RadioButton B15 = findViewById(R.id.radioButtonB15);
+        final RadioButton C1 = findViewById(R.id.radioButtonC1);
+        final RadioButton C2 = findViewById(R.id.radioButtonC2);
+        final RadioButton C3 = findViewById(R.id.radioButtonC3);
+        final RadioButton C4 = findViewById(R.id.radioButtonC4);
+        final RadioButton C5 = findViewById(R.id.radioButtonC5);
+        final RadioButton C6 = findViewById(R.id.radioButtonC6);
+        final RadioButton C7 = findViewById(R.id.radioButtonC7);
+        final RadioButton C8 = findViewById(R.id.radioButtonC8);
+        final RadioButton C9 = findViewById(R.id.radioButtonC9);
+        final RadioButton C10 = findViewById(R.id.radioButtonC10);
+        final RadioButton C11 = findViewById(R.id.radioButtonC11);
+        final RadioButton C12 = findViewById(R.id.radioButtonC12);
+        final RadioButton C13 = findViewById(R.id.radioButtonC13);
+        final RadioButton C14 = findViewById(R.id.radioButtonC14);
+        final RadioButton C15 = findViewById(R.id.radioButtonC15);
+
+
+
+
+        A1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ocistiRadioButtons();
+                C2.setChecked(true);
+                B2.setChecked(true);
+                C7.setChecked(true);
+
+            }
+        });
+        A2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                C8.setChecked(true);
+                B5.setChecked(true);
+                C15.setChecked(true);
+
+                C2.setChecked(false);
+                B2.setChecked(false);
+                C7.setChecked(false);
+
+            }
+        });
+        A3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        C15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+}
